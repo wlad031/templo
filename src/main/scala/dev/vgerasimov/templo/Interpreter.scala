501cf9c049045100d2bd4d554a078774a3e7bc0c
Vladislav Gerasimov <gerasimovvs@yahoo.com>
2023-01-16 23:33:36 +0100

    Microrefactoring

--- src/main/scala/dev/vgerasimov/templo/Interpreter.scala
+++ src/main/scala/dev/vgerasimov/templo/Interpreter.scala
@@ -11,8 +11,16 @@ object Interpreter:
 object LizpInterpreter extends Interpreter:
   import dev.vgerasimov.lizp.{ Reader as KekR, Interpreter as KekI, Parser as KekP, * }
 
+  private object CommonBlocks:
+    private val INCLUDE_STD = Block.Code("(include std)")
+    private val CONCAT_START = Block.Code("(+")
+    private val CONCAT_END = Block.Code(")")
+
+    def wrap(blocks: List[Block]): List[Block] = 
+      INCLUDE_STD :: CONCAT_START :: (blocks ++ List(CONCAT_END))
+
   override def apply(blocks: List[Block]): Either[Interpreter.Error, String] =
-    val source = (Block.Code("(include std)(+ ") :: (blocks ++ List(Block.Code(")")))).map {
+    val source = CommonBlocks.wrap(blocks).map {
       case Block.Code(code) => code
       case Block.Text(text) => s""""$text""""
     }.mkString(" ")
